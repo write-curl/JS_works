ef2ca2444303e86f423ca82943bdbfb6a11125c5
write-curl <writingprocrastination@gmail.com>
2026-06-12 13:03:39 -0400

    created objects.js for lab exercise

---
+++ objects.js
@@ -0,0 +1,21 @@
+//objects lab, build a person with object literal notation
+//variable 'human' using boolean, number, and string
+var humanBeing = {
+    hungry: true,
+    age: 25,
+    height: '5\'10'
+};
+console.log (humanBeing)
+//variable 'pizza' using dot notation (object.property = value)
+var pizza= {};
+pizza.crust = 'sourdough';
+pizza.sauce = 'marinara';
+pizza.cheese = 'mozzarella';
+pizza.toppings = 'pepperoni, mushrooms';
+console.log(pizza)
+//variable 'cup' using bracket notation (object['property'] = value)
+var cup = {};
+cup['insulated'] = true;
+cup['liquid'] = 'water';
+cup['oz'] = 16;
+console.log(cup)
