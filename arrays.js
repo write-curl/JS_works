ae17b0e48de7ca20c9681ff4af0ba84cd352ae8e
write-curl <writingprocrastination@gmail.com>
2026-06-17 11:33:50 -0400

    added array syntax lab

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var testScores = [];
+//elements of arrays
+testScores = [72, 84, 68, 92, 74];
+//index of arrays
+//console.log (testScores[0])
+
+//Array syntax lab
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros'];
+console.log (baseballTeams[1])
+baseballTeams[5] = 'Rays';
+baseballTeams[1] = 'Padres';
+var myFavoriteTeam = baseballTeams[0];
+console.log (myFavoriteTeam)
+console.log (baseballTeams)
+var californiaBaseball = baseballTeams[1];
+console.log (californiaBaseball)
+
+
