549eef00ff0dab7a43c8f7f627f89da354e0aba4
write-curl <writingprocrastination@gmail.com>
2026-06-18 11:03:26 -0400

    adjusted variables for console.log statement

--- insultGenerator.js
+++ insultGenerator.js
@@ -6,11 +6,11 @@ var randomAdjectives = ['bored', 'cheeky', 'sharp', 'ludicrous'];
 var randomWords = ['ripper', 'breeze', 'lychee', 'gimlet', 'wolf'];
 //console.log (randomWords)
 //Use math.floor() and math.random() methods on each variable
-var randomBodyParts = randomBodyParts[Math.floor(Math.random() * 4)];
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 4)];
 //console.log (randomBodyParts)
-var randomAdjectives = randomAdjectives[Math.floor(Math.random() * 4)];
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 4)];
 //console.log (randomAdjectives)
-var randomWords = randomWords[Math.floor(Math.random() * 5)];
+var randomWord = randomWords[Math.floor(Math.random() * 5)];
 //console.log (randomWords)
 //create a console.log statement that displays the randomly generated insult
-console.log ('Your ' + randomBodyParts + ' is like a ' + randomAdjectives + ' ' + randomWords + '!!!')
+console.log ('Your ' + randomBodyPart + ' is like a ' + randomAdjective + ' ' + randomWord + '!!!')
