1bba0919ffb1d76f8ba1221ca8c392661e0c941e
write-curl <writingprocrastination@gmail.com>
2026-06-26 13:34:17 -0400

    added while, do/while, and for loop exercises

--- loopsLab.js
+++ loopsLab.js
@@ -5,10 +5,10 @@
 // (false)
 
 //solutions
-console.log (5<7)
-console.log (7 == '7')
-console.log (7 != 'seven')
-console.log (true)
+// console.log (5<7)
+// console.log (7 == '7')
+// console.log (7 != 'seven')
+// console.log (true)
 
 // use logical operators && and || to show four different code examples for each
 // && (and), || (or)
@@ -36,6 +36,40 @@ if(uvIndex > 6) {
     } else {
         console.log ('It is night time')
     }
+
+// create a while loop that runs 5x's
+var runs = 1
+while (runs < 7) {
+    console.log ('The team has ' + (runs+1) + ' runs.');
+    runs++;
+}
+
+// create a do/while loop that runs with a false expression
+var x = 2
+do {
+    console.log('false expression.');
+    x++;
+    } while (x < 7);
+
+// create a for loop with an array of 5 values, run each element through an if, else, else if clause with at least two else if options
+var pitchSpeed = [101, 95, 97, 88, 92, 87, 94, 90, 99]
+
+for (var i = 0; i < pitchSpeed.length; i++) {
+    // console.log('This pitcher throws ' + pitchSpeed[i] + '.');
+if (pitchSpeed[i] > 98) {
+    console.log ('This pitcher throws heat')
+    } else if (pitchSpeed[i] > 95) {
+        console.log ('This pitcher has strikeout capability')
+    } else if (pitchSpeed[i] > 92) {
+        console.log ('This pitcher is sneaky')
+    } else {
+        console.log ('This pitcher throws meatballs.')
+    }
+}
+
+
+
+
 
 
 
